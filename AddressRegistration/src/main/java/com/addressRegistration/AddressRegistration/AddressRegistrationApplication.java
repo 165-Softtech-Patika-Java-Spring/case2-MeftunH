587d95621a82cc1485bfecef01697ae878ac6bf4
MeftunH <meftun.hesimli@epigra.com>
2022-02-21 01:57:34 +0300

    fix lombok and mapper

--- AddressRegistration/src/main/java/com/addressRegistration/AddressRegistration/AddressRegistrationApplication.java
+++ AddressRegistration/src/main/java/com/addressRegistration/AddressRegistration/AddressRegistrationApplication.java
@@ -2,12 +2,29 @@ package com.addressRegistration.AddressRegistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 
-@SpringBootApplication
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootApplication()
 public class AddressRegistrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AddressRegistrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AddressRegistrationApplication.class, args);
+    }
+    @Bean
+    public LinkDiscoverers discovers() {
 
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
 }
