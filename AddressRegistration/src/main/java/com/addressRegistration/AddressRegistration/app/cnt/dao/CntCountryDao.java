96b26a035e2f776d180c30982e129a928546b815
MeftunH <meftun.hesimli@epigra.com>
2022-02-21 02:44:37 +0300

    remove unused imports

--- AddressRegistration/src/main/java/com/addressRegistration/AddressRegistration/app/cnt/dao/CntCountryDao.java
+++ AddressRegistration/src/main/java/com/addressRegistration/AddressRegistration/app/cnt/dao/CntCountryDao.java
@@ -1,8 +1,6 @@
 package com.addressRegistration.AddressRegistration.app.cnt.dao;
 
 import com.addressRegistration.AddressRegistration.app.cnt.entity.CntCountryEntity;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
