531ba0b7b1b312785c614026369e77ab9ae5e264
MeftunH <meftun.hesimli@epigra.com>
2022-02-21 03:10:36 +0300

    remove unused imports

--- AddressRegistration/src/main/java/com/addressRegistration/AddressRegistration/app/cnt/converter/CntCountryMapper.java
+++ AddressRegistration/src/main/java/com/addressRegistration/AddressRegistration/app/cnt/converter/CntCountryMapper.java
@@ -5,16 +5,14 @@ import com.addressRegistration.AddressRegistration.app.cnt.entity.CntCountryEnti
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
-//import org.mapstruct.factory.Mappers;
-import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CntCountryMapper {
 
     CntCountryMapper INSTANCE = Mappers.getMapper(CntCountryMapper.class);
 
-    CntCountryEntity convertToCntCountry(CntCountryDto prdProductSaveRequestDto);
+    CntCountryEntity convertToCntCountry(CntCountryDto cntCountrySaveRequestDto);
 
-    CntCountryDto convertToCntCountryDto(CntCountryEntity prdProductSaveRequestDto);
+    CntCountryDto convertToCntCountryDto(CntCountryEntity cntCountrySaveRequestDto);
 
 }
